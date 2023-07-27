ea539155de1049992f2eeb639b9682e06e8d321f
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2023-07-27 21:34:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/charlie /charlie
+  git clone https://github.com/dmxbasil/Tom-cruise-v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /charlie
